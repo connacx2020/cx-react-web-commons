1e35281c2ceead6e17b0aa00890dd290a43d5043
Oak Soe <oaksoekyaw@gmail.com>
2020-01-06 01:16:15 +0800

    testing react ts package

--- packages/cx-react-web-core/src/components/login/login.tsx
+++ packages/cx-react-web-core/src/components/login/login.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Button, TextField, Card, CardHeader } from '@material-ui/core';
-import Auth from '../../interfaces/auth.interface';
-import './login.scss';
+// import { Button, TextField, Card, CardHeader } from '@material-ui/core';
+// import Auth from '../../interfaces/auth.interface';
+// import './login.scss';
 
 class Login extends Component<{}, {
     email: string,
@@ -17,22 +17,28 @@ class Login extends Component<{}, {
     }
 
     render() {
-        const { email, password } = this.state;
-            
-        //onSubmit={this.loginSubmit}
         return (
-            <div className="DN_Login_Container">
-                <Card className="card">
-                    <form className="DN_Login_Form_Container">
-                        <CardHeader title="DevNet" />
-                        <TextField id="outlined-basic" className="email_input" label="Email" variant="outlined" value={email} onChange={this.emailTextChanged} />
-                        <TextField id="outlined-basic" type="password" className="password_input" label="Password" variant="outlined" value={password} onChange={this.passwordTextChanged} />
-                        <Button type='submit' className="login_btn" variant="contained" color="primary">Login</Button>
-                    </form>
-                </Card>
-            </div>
+            <div>login</div>
         )
     }
+
+    // render() {
+    //     const { email, password } = this.state;
+            
+    //     //onSubmit={this.loginSubmit}
+    //     return (
+    //         <div className="DN_Login_Container">
+    //             <Card className="card">
+    //                 <form className="DN_Login_Form_Container">
+    //                     <CardHeader title="DevNet" />
+    //                     <TextField id="outlined-basic" className="email_input" label="Email" variant="outlined" value={email} onChange={this.emailTextChanged} />
+    //                     <TextField id="outlined-basic" type="password" className="password_input" label="Password" variant="outlined" value={password} onChange={this.passwordTextChanged} />
+    //                     <Button type='submit' className="login_btn" variant="contained" color="primary">Login</Button>
+    //                 </form>
+    //             </Card>
+    //         </div>
+    //     )
+    // }
 
     // loginSubmit = event => {
     //     // console.log(this.state.email + '\n' + this.state.password)
